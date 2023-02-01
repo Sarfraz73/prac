2fffdc6fb94a3441b588275803ab85fde31a033e
Sarfraj shaikh <shaikhsarfraz930@gmail.com>
2023-02-02 01:11:41 +0530

    for diff java

--- Sum.java
+++ Sum.java
@@ -3,10 +3,9 @@ public class Sum{
     public static void main(String args[])
     {
         int a=10;
-        int b=20;
+        int b=20; 
         int c=a+b;
         System.out.println(c);
-        System.out.println("changes");
-
+ 
     }
 }
